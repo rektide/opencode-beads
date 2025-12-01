ee09750b76a09c76c7f2c44216511b8b46972d7a
rektide de la faye <rektide@voodoowarez.com>
2025-12-01 03:47:32 -0500

    get familiar with basic tool definition

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ An exploration of a OpenCode plugin for [Beads](https://steve-yegge.medium.com/i
 # Installation
 
 For now, manually copy or link `beads-opencode.ts` into `~/.config/opencode/plugin/beads-opencode.ts`.
+
+# Tools
+
+## Navigation
+
+Dumping the entire Beads issue-tracking into OpenCode's Todo is almost certainly not what we want: in many cases we expect that to be far too big. Nav is a set of tools for discovery, and which more, like a directory, it helps us to walk into the tree of tasks, to an area of focus. Other tools will likely be constrained by the nav tool.
--- opencode-beads.ts
+++ opencode-beads.ts
@@ -1,1 +1,65 @@
-import { x } from "tinyexec";
+import type { ToolContext } from "@opencode-ai/plugin"
+import { tool } from "@opencode-ai/plugin"
+import { x } from "tinyexec"
+
+export const explore = tool({
+	description: "Explore how plugin integration works in opencode",
+	args: {},
+	async execute(args, context: ToolContext) {
+		const { agent, sessionID, messageID } = context
+		context.sessionID
+		return `Agent: ${agent}, Session: ${sessionID}, Message: ${messageID}, other: ${Object.keys(
+			context,
+		).join(",")}`
+	},
+})
+
+interface NavState {
+	parentId: string
+	ancestorIds: Iterator<string>
+}
+// TODO: create an epic for navState handling. learn how to use/beg OpenCode storage for holding state information, if at all possible.
+const navState: Record<string, NavState> = {}
+
+export const ready = tool({
+	description: "List ready tasks",
+	args: {
+		// TODO: zod schema for beads viewing ready issues here. Include all filters a user can add with `bd ready`.
+	},
+	async execute(args: Record<string, never>, context: ToolContext) {
+		const params = ["ready"]
+		// TODO: add all filters from args as parameters here
+		// TODO: add `navState` filters for any filters not set by args
+		const result = await x("bd", params)
+
+		if (result.exitCode) {
+			throw new Error(`bd failed when trying to get a list of ready tasks`)
+		}
+
+		return result.stdout
+	},
+})
+
+export const create_task = tool({
+	description: "Create a new epic",
+	args: {
+		// TODO: zod schema for all options available to `bd create` issue creation.
+	},
+	async execute(args: Record<string, never>, context: ToolContext) {
+		const params = ["ready"]
+		// TODO: add all filters from args as parameters here
+		// TODO: look at `navState` and set our parent id to the parent id there
+		const result = await x("bd", params)
+
+		if (result.exitCode) {
+			throw new Error(`bd failed when trying to get a list of ready tasks`)
+		}
+
+		// TODO: the stdout should include the new task id. set the navState task id to
+		return result.stdout
+	},
+})
+
+// TODO: add `list` tool.
+
+// TODO add a "load_todos" tool to load all tasks in the current nav into OpenCode todos
